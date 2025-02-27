c8b840133aa73efe5b8120c3c59c92f87235f253
Paulina <paulina.labrinl@usm.cl>
2025-02-26 22:51:10 -0300

    cambios inicio

--- frontend/src/Inicio/Inicio.jsx
+++ frontend/src/Inicio/Inicio.jsx
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
-import {  BrowserRouter, Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { BiCloudUpload, BiWrench, BiBell, BiTrendingUp } from "react-icons/bi";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 
 import "./Inicio.css";
 import LogoAzul from "./LogoS_Azul.png";
@@ -28,6 +34,7 @@ const Inicio = () => {
 
     fetchServices();
   }, []);
+
   const parseDescription = (text) => {
     // Divide el texto en partes que coincidan con *algo*
     return text.split(/(\*[^*]+\*)/g).map((segment, index) => {
@@ -38,21 +45,42 @@ const Inicio = () => {
       return segment;
     });
   };
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    centerMode: true,
+    arrows: true,
+    slidesToShow: 3, // Mostrar 3 tarjetas a la vez
+    autoplay: true,
+    autoplaySpeed: 10000,
+    beforeChange: (current, next) => setActiveIndex(next), // Detecta cambio de slide
+    responsive: [
+      { breakpoint: 1024, settings: { slidesToShow: 2 } },
+      { breakpoint: 768, settings: { slidesToShow: 1 } },
+    ],
+  };
+
+  useEffect(() => {
+    AOS.init({ duration: 1000, once: true });
+  }, []);
   
     return (
       <>
         <div className="inicio-container">
-          <div className="inicio-text">
+          <div className="inicio-text" data-aos="slide-right">
             <h1>Lideramos la transformación empresarial hacia un futuro de excelencia y crecimiento sostenible.</h1>
-            <button className="btn-servicios">Conoce nuestros servicios</button>
+            <NavLink to="/servicios" className="btn-servicios">Conoce nuestros servicios</NavLink>
           </div>
-          <div className="inicio-imagen">
+          <div className="inicio-imagen" data-aos="slide-left">
             <img src={LogoAzul} alt="Sistemgraf" />
           </div>
         </div>
         
         <div className="modServicio">
-          <div className="modServicio-container">
+          <div className="modServicio-container" data-aos="fade-up">
             <h2 className="titulo">Modalidad de Servicio</h2>
             <p className="subtitulo">Ofrecemos un servicio "SaaS", que permite obtener</p>
 
@@ -85,7 +113,7 @@ const Inicio = () => {
         </div>
 
         <div className="acercaDe-container">
-          <div className="acercaDe-content">
+          <div className="acercaDe-content" data-aos="fade-up">
             {/* Imagen de la empresa o el gato */}
             <div className="acercaDe-imagen">
               <img src={ Gato } alt="Acerca de Sistemgraf" />
@@ -103,41 +131,43 @@ const Inicio = () => {
               </p>
               
               {/* Botón Leer más */}
-              <button className="btn-leer-mas">Leer más</button>
+              <NavLink to="/nosotros" className="btn-leer-mas">Leer más</NavLink>
             </div>
           </div>
         </div>
 
-        <div className="soluciones-container">
-          <div className="servicios-grid">
-            {servicios.map((servicio, ID) => (
-              <div key={ID} className="servicio-item">
-                <div className="servicio-card-custom">
-                  <div className="servicio-header">
-                      {/* Contenedor de la imagen */}
-                      <div className="servicio-img-container">
-                          <img
-                              src={servicio.Img}
-                              alt="Imagen del servicio"
-                              className="servicio-img-custom"
-                          />
-                      </div>
-                      
-                      {/* Contenedor del título */}
-                      <div className="servicio-title-container">
-                          <h5 className="servicio-title">{servicio.Name}</h5>
-                      </div>
+        <div className="soluciones-container" >
+          <h2 className="titulo-soluciones" data-aos="fade-up">Nuestras Soluciones</h2>
+          <Slider  {...settings}>
+            {servicios.map((servicio, index) => (
+              <div
+                key={index}
+                className={`servicio-item ${index === activeIndex ? "active" : ""}`}
+              >
+                <div className="servicio-card" data-aos="fade-up">
+                  {/* Contenedor de la imagen */}
+                  <div className="servicio-img-container">
+                    <img
+                      src={servicio.Img}
+                      alt="Imagen del servicio"
+                      className="servicio-img"
+                    />
+                  </div>
+
+                  {/* Contenedor del título */}
+                  <div className="servicio-title-container">
+                    <h5 className="servicio-title">{servicio.Name}</h5>
                   </div>
 
                   {/* Caja inferior con descripción */}
                   <div className="servicio-content">
                     <p className="servicio-description">{servicio.Description}</p>
-                    <button className="btn-mas-info">Leer más</button>
+                    <NavLink to="/servicios" className="btn-mas-info">Leer más </NavLink>
                   </div>
                 </div>
               </div>
             ))}
-          </div>
+          </Slider>
         </div>
 
         
