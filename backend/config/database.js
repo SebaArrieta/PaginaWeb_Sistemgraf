17d066f63afb2bf29dfc5d5c1de8014f2f006348
SebaArrieta <sebastian.arrieta@usm.cl>
2025-04-02 20:07:56 -0300

    commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,25 +2,28 @@ const mysql = require('mysql2');
 const fs = require('fs');
 
 // Configura los parámetros de la base de datos
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQL_PORT,
   ssl: {
-      ca: fs.readFileSync(process.env.CA_CERT), // Your CA certificate content
-      rejectUnauthorized: true, // Ensures the client validates the server's certificate
-  }
+    ca: fs.readFileSync(process.env.CA_CERT),
+    rejectUnauthorized: true,
+  },
+  waitForConnections: true,
+  connectionLimit: 10,     // Ajusta el límite según tus necesidades
+  queueLimit: 0
 });
 
 // Conecta a la base de datos
-connection.connect((err) => {
+/*connection.connect((err) => {
   if (err) {
     console.error('Error conectando a la base de datos:', err);
     return;
   }
   console.log('Conectado a la base de datos MySQL en DigitalOcean');
-});
+});*/
 
 module.exports = connection;
